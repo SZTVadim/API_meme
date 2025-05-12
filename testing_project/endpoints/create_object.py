74b56761772a70583857536f3d8a744ea65f2f85
Vadim <vadimarmo@gmail.com>
2025-05-12 10:44:11 +0300

    version 0.7

--- testing_project/endpoints/create_object.py
+++ testing_project/endpoints/create_object.py
@@ -10,5 +10,4 @@ class CreateMeme(Endpoint):
         if self.response.status_code == 200:
             self.response_json = self.response.json()
             self.meme_id = self.response_json["id"]
-            print(self.meme_id)
         return self.meme_id
